b653712ef787ca24b6c82fa9d1fc913f4b24bae6
Daniel Roth <daniel.roth@mailbase.info>
2023-10-03 14:49:29 +0200

    Fixes a bug where the same user got multiple fcm notifications
    * multiple of the same fcm tokens were stored per user
      -> make token and userId the primary key

---
+++ Migrations/20231003122614_FcmTokensPrimaryKey.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingListServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class FcmTokensPrimaryKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FcmToken_Users_UserId",
+                table: "FcmToken");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_FcmToken",
+                table: "FcmToken");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FcmToken_UserId",
+                table: "FcmToken");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "FcmToken");
+
+            migrationBuilder.UpdateData(
+                table: "FcmToken",
+                keyColumn: "UserId",
+                keyValue: null,
+                column: "UserId",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "FcmToken",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "FcmToken",
+                keyColumn: "Token",
+                keyValue: null,
+                column: "Token",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "FcmToken",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_FcmToken",
+                table: "FcmToken",
+                columns: new[] { "UserId", "Token" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FcmToken_Users_UserId",
+                table: "FcmToken",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FcmToken_Users_UserId",
+                table: "FcmToken");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_FcmToken",
+                table: "FcmToken");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "FcmToken",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "FcmToken",
+                type: "varchar(255)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "FcmToken",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_FcmToken",
+                table: "FcmToken",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FcmToken_UserId",
+                table: "FcmToken",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FcmToken_Users_UserId",
+                table: "FcmToken",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
--- Models/FcmToken.cs
+++ Models/FcmToken.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 using ShoppingListServer.Entities;
 
 namespace ShoppingListServer.Models
 {
+    /// <summary>
+    /// A token that is used to send notifications to a device using fcm.
+    /// A token is connected to a user. There can be multiple tokens
+    /// per user but there should only be one token per device.
+    /// (A user has multiple tokens if they are logged in on different devices.)
+    /// </summary>
+    [PrimaryKey(nameof(UserId), nameof(Token))]
     public class FcmToken
     {
-        [Key, Required]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-
         // Foreign key for 1:n relationship
         public string UserId { get; set; }
         public virtual User User { get; set; }
